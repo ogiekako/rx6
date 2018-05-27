ea28e1eac54eb5d7a5a48b01d38dbb1adcf3fa97
Keigo Oka <ogiekako@gmail.com>
2018-05-27 09:51:08 +0900

    WIP

--- kern/src/kernmain.rs
+++ kern/src/kernmain.rs
@@ -38,13 +38,78 @@ pub unsafe fn kernmain() {
     // if(!ismp)
     //   timerinit();   // uniprocessor timer (TODO)
     startothers(); // start other processors
-    kalloc::kinit2(p2v(P(4 * 1024 * 1024)), p2v(PHYSTOP)); // must come after startothers() (TODO)
-    userinit(); // first user process (TODO)
+    kalloc::kinit2(p2v(P(4 * 1024 * 1024)), p2v(PHYSTOP)); // must come after startothers()
+    process::userinit(); // first user process (TODO)
     mpmain(); // finish this processor's setup (TODO)
     console::cprintf("looping\n", &[]);
     loop {}
 }
 
-fn startothers() {}
-fn userinit() {}
-fn mpmain() {}
+// // Other CPUs jump here from entryother.S.
+// static void
+// mpenter(void)
+// {
+//   switchkvm();
+//   seginit();
+//   lapicinit();
+//   mpmain();
+// }
+ 
+// Common CPU setup code.
+fn mpmain() {
+//  cprintf("cpu%d: starting %d\n", cpuid(), cpuid());
+//  idtinit();       // load idt register
+//  xchg(&(mycpu()->started), 1); // tell startothers() we're up
+//  scheduler();     // start running processes
+}
+
+// Start the non-boot (AP) processors.
+fn startothers()
+{
+//  extern uchar _binary_entryother_start[], _binary_entryother_size[];
+//  uchar *code;
+//  struct cpu *c;
+//  char *stack;
+//
+//  // Write entry code to unused memory at 0x7000.
+//  // The linker has placed the image of entryother.S in
+//  // _binary_entryother_start.
+//  code = P2V(0x7000);
+//  memmove(code, _binary_entryother_start, (uint)_binary_entryother_size);
+//
+//  for(c = cpus; c < cpus+ncpu; c++){
+//    if(c == mycpu())  // We've started already.
+//      continue;
+//
+//    // Tell entryother.S what stack to use, where to enter, and what
+//    // pgdir to use. We cannot use kpgdir yet, because the AP processor
+//    // is running in low  memory, so we use entrypgdir for the APs too.
+//    stack = kalloc();
+//    *(void**)(code-4) = stack + KSTACKSIZE;
+//    *(void**)(code-8) = mpenter;
+//    *(int**)(code-12) = (void *) V2P(entrypgdir);
+//
+//    lapicstartap(c->apicid, V2P(code));
+//
+//    // wait for cpu to finish mpmain()
+//    while(c->started == 0)
+//      ;
+//  }
+}
+
+// // The boot page table used in entry.S and entryother.S.
+// // Page directories (and page tables) must start on page boundaries,
+// // hence the __aligned__ attribute.
+// // PTE_PS in a page directory entry enables 4Mbyte pages.
+// 
+// __attribute__((__aligned__(PGSIZE)))
+// pde_t entrypgdir[NPDENTRIES] = {
+//   // Map VA's [0, 4MB) to PA's [0, 4MB)
+//   [0] = (0) | PTE_P | PTE_W | PTE_PS,
+//   // Map VA's [KERNBASE, KERNBASE+4MB) to PA's [0, 4MB)
+//   [KERNBASE>>PDXSHIFT] = (0) | PTE_P | PTE_W | PTE_PS,
+// };
+// 
+// //PAGEBREAK!
+// // Blank page.
+// //PAGEBREAK!
--- kern/src/process.rs
+++ kern/src/process.rs
@@ -207,45 +207,42 @@ pub unsafe fn myproc() -> &'static mut Proc {
 //
 //   return p;
 // }
+
+// Set up first user process.
+pub unsafe fn userinit() {
+//  struct proc *p;
+//  extern char _binary_initcode_start[], _binary_initcode_size[];
 //
-// //PAGEBREAK: 32
-// // Set up first user process.
-// void
-// userinit(void)
-// {
-//   struct proc *p;
-//   extern char _binary_initcode_start[], _binary_initcode_size[];
+//  p = allocproc();
 //
-//   p = allocproc();
+//  initproc = p;
+//  if((p->pgdir = setupkvm()) == 0)
+//    panic("userinit: out of memory?");
+//  inituvm(p->pgdir, _binary_initcode_start, (int)_binary_initcode_size);
+//  p->sz = PGSIZE;
+//  memset(p->tf, 0, sizeof(*p->tf));
+//  p->tf->cs = (SEG_UCODE << 3) | DPL_USER;
+//  p->tf->ds = (SEG_UDATA << 3) | DPL_USER;
+//  p->tf->es = p->tf->ds;
+//  p->tf->ss = p->tf->ds;
+//  p->tf->eflags = FL_IF;
+//  p->tf->esp = PGSIZE;
+//  p->tf->eip = 0;  // beginning of initcode.S
 //
-//   initproc = p;
-//   if((p->pgdir = setupkvm()) == 0)
-//     panic("userinit: out of memory?");
-//   inituvm(p->pgdir, _binary_initcode_start, (int)_binary_initcode_size);
-//   p->sz = PGSIZE;
-//   memset(p->tf, 0, sizeof(*p->tf));
-//   p->tf->cs = (SEG_UCODE << 3) | DPL_USER;
-//   p->tf->ds = (SEG_UDATA << 3) | DPL_USER;
-//   p->tf->es = p->tf->ds;
-//   p->tf->ss = p->tf->ds;
-//   p->tf->eflags = FL_IF;
-//   p->tf->esp = PGSIZE;
-//   p->tf->eip = 0;  // beginning of initcode.S
+//  safestrcpy(p->name, "initcode", sizeof(p->name));
+//  p->cwd = namei("/");
 //
-//   safestrcpy(p->name, "initcode", sizeof(p->name));
-//   p->cwd = namei("/");
+//  // this assignment to p->state lets other cores
+//  // run this process. the acquire forces the above
+//  // writes to be visible, and the lock is also needed
+//  // because the assignment might not be atomic.
+//  acquire(&ptable.lock);
 //
-//   // this assignment to p->state lets other cores
-//   // run this process. the acquire forces the above
-//   // writes to be visible, and the lock is also needed
-//   // because the assignment might not be atomic.
-//   acquire(&ptable.lock);
+//  p->state = RUNNABLE;
 //
-//   p->state = RUNNABLE;
-//
-//   release(&ptable.lock);
-// }
-//
+//  release(&ptable.lock);
+}
+
 // // Grow current process's memory by n bytes.
 // // Return 0 on success, -1 on failure.
 // int
