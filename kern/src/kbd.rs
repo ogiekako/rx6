1ffba35421c70a338bbb90c7136a2cbee266ad78
Keigo Oka <ogiekako@gmail.com>
2019-05-04 17:47:49 +0900

    Compile kbd.rs

--- kern/src/kbd.rs
+++ kern/src/kbd.rs
@@ -1,3 +1,4 @@
+use super::*;
 // kbd.h
 
 // PC keyboard interface constants
@@ -35,48 +36,66 @@ const fn C(x: u8) -> u8 {
     x.wrapping_sub(b'@')
 }
 
-//// static uchar shiftcode[256] =
-//// {
-////   [0x1D] CTL,
-////   [0x2A] SHIFT,
-////   [0x36] SHIFT,
-////   [0x38] ALT,
-////   [0x9D] CTL,
-////   [0xB8] ALT
-//// };
-////
-//// static uchar togglecode[256] =
-//// {
-////   [0x3A] CAPSLOCK,
-////   [0x45] NUMLOCK,
-////   [0x46] SCROLLLOCK
-//// };
-////
-//// static uchar normalmap[256] =
-//// {
-////   NO,   0x1B, '1',  '2',  '3',  '4',  '5',  '6',  // 0x00
-////   '7',  '8',  '9',  '0',  '-',  '=',  '\b', '\t',
-////   'q',  'w',  'e',  'r',  't',  'y',  'u',  'i',  // 0x10
-////   'o',  'p',  '[',  ']',  '\n', NO,   'a',  's',
-////   'd',  'f',  'g',  'h',  'j',  'k',  'l',  ';',  // 0x20
-////   '\'', '`',  NO,   '\\', 'z',  'x',  'c',  'v',
-////   'b',  'n',  'm',  ',',  '.',  '/',  NO,   '*',  // 0x30
-////   NO,   ' ',  NO,   NO,   NO,   NO,   NO,   NO,
-////   NO,   NO,   NO,   NO,   NO,   NO,   NO,   '7',  // 0x40
-////   '8',  '9',  '-',  '4',  '5',  '6',  '+',  '1',
-////   '2',  '3',  '0',  '.',  NO,   NO,   NO,   NO,   // 0x50
-////   [0x9C] '\n',      // KP_Enter
-////   [0xB5] '/',       // KP_Div
-////   [0xC8] KEY_UP,    [0xD0] KEY_DN,
-////   [0xC9] KEY_PGUP,  [0xD1] KEY_PGDN,
-////   [0xCB] KEY_LF,    [0xCD] KEY_RT,
-////   [0x97] KEY_HOME,  [0xCF] KEY_END,
-////   [0xD2] KEY_INS,   [0xD3] KEY_DEL
-//// };
+fn shiftcode(x: u8) -> u8 {
+    match x {
+        0x1D => CTL,
+        0x2A => SHIFT,
+        0x36 => SHIFT,
+        0x38 => ALT,
+        0x9D => CTL,
+        0xB8 => ALT,
+        _ => 0,
+    }
+}
+
+fn togglecode(x: u8) -> u8 {
+    match x {
+        0x3A => CAPSLOCK,
+        0x45 => NUMLOCK,
+        0x46 => SCROLLLOCK,
+        _ => 0,
+    }
+}
 
 #[rustfmt::skip]
-static shiftmap: [u8;256] = [
-  NO,   033 /* esc */,  b'!',  b'@',  b'#',  b'$',  b'%',  b'^',  // 0x00
+const normalmap: [u8; 256] = [
+  NO,   0x1B, b'1',  b'2',  b'3',  b'4',  b'5',  b'6',  // 0x00
+  b'7',  b'8',  b'9',  b'0',  b'-',  b'=',  8 /* bs */, b'\t',
+  b'q',  b'w',  b'e',  b'r',  b't',  b'y',  b'u',  b'i',  // 0x10
+  b'o',  b'p',  b'[',  b']',  b'\n', NO,   b'a',  b's',
+  b'd',  b'f',  b'g',  b'h',  b'j',  b'k',  b'l',  b';',  // 0x20
+  b'\'', b'`',  NO,   b'\\', b'z',  b'x',  b'c',  b'v',
+  b'b',  b'n',  b'm',  b',',  b'.',  b'/',  NO,   b'*',  // 0x30
+  NO,   b' ',  NO,   NO,   NO,   NO,   NO,   NO,
+  NO,   NO,   NO,   NO,   NO,   NO,   NO,   b'7',  // 0x40
+  b'8',  b'9',  b'-',  b'4',  b'5',  b'6',  b'+',  b'1',
+  b'2',  b'3',  b'0',  b'.',  NO,   NO,   NO,   NO,   // 0x50
+  NO, NO, NO, NO, NO, NO, NO, NO,
+  NO, NO, NO, NO, NO, NO, NO, NO, // 0x60
+  NO, NO, NO, NO, NO, NO, NO, NO,
+  NO, NO, NO, NO, NO, NO, NO, NO, // 0x70
+  NO, NO, NO, NO, NO, NO, NO, NO,
+  NO, NO, NO, NO, NO, NO, NO, NO, // 0x80
+  NO, NO, NO, NO, NO, NO, NO, NO,
+  NO, NO, NO, NO, NO, NO, NO, KEY_HOME, // 0x90
+  NO, NO, NO, NO, b'\n' /* KP_Enter */, NO, NO, NO,
+  NO, NO, NO, NO, NO, NO, NO, NO, // 0xa0
+  NO, NO, NO, NO, NO, NO, NO, NO,
+  NO, NO, NO, NO, NO, b'/' /* KP_Div */ , NO, NO, // 0xb0
+  NO, NO, NO, NO, NO, NO, NO, NO,
+  NO, NO, NO, NO, NO, NO, NO, NO, // 0xc0
+  KEY_UP, KEY_PGUP, NO, KEY_LF, NO, KEY_RT, NO, KEY_END,
+  KEY_DN, KEY_PGDN, KEY_INS, KEY_DEL, NO, NO, NO, NO, // 0xd0
+  NO, NO, NO, NO, NO, NO, NO, NO,
+  NO, NO, NO, NO, NO, NO, NO, NO, // 0xe0
+  NO, NO, NO, NO, NO, NO, NO, NO,
+  NO, NO, NO, NO, NO, NO, NO, NO, // 0xf0
+  NO, NO, NO, NO, NO, NO, NO, NO,
+];
+
+#[rustfmt::skip]
+const shiftmap: [u8; 256] = [
+  NO,   0o33 /* esc */,  b'!',  b'@',  b'#',  b'$',  b'%',  b'^',  // 0x00
   b'&',  b'*',  b'(',  b')',  b'_',  b'+',  8 /* Backspace */, b'\t',
   b'Q',  b'W',  b'E',  b'R',  b'T',  b'Y',  b'U',  b'I',  // 0x10
   b'O',  b'P',  b'{',  b'}',  b'\n', NO,   b'A',  b'S',
@@ -111,7 +130,7 @@ static shiftmap: [u8;256] = [
 ];
 
 #[rustfmt::skip]
-static ctlmap: [u8; 256] = [
+const ctlmap: [u8; 256] = [
     NO, NO, NO, NO, NO, NO, NO, NO, // 0x00
     NO, NO, NO, NO, NO, NO, NO, NO,
     C(b'Q'), C(b'W'), C(b'E'), C(b'R'), C(b'T'), C(b'Y'), C(b'U'), C(b'I'), // 0x10
@@ -147,54 +166,47 @@ static ctlmap: [u8; 256] = [
 ];
 
 // kbd.c
-//
-//// #include "types.h"
-//// #include "x86.h"
-//// #include "defs.h"
-//// #include "kbd.h"
-////
-//// int
-//// kbdgetc(void)
-//// {
-////   static uint shift;
-////   static uchar *charcode[4] = {
-////     normalmap, shiftmap, ctlmap, ctlmap
-////   };
-////   uint st, data, c;
-////
-////   st = inb(KBSTATP);
-////   if((st & KBS_DIB) == 0)
-////     return -1;
-////   data = inb(KBDATAP);
-////
-////   if(data == 0xE0){
-////     shift |= E0ESC;
-////     return 0;
-////   } else if(data & 0x80){
-////     // Key released
-////     data = (shift & E0ESC ? data : data & 0x7F);
-////     shift &= ~(shiftcode[data] | E0ESC);
-////     return 0;
-////   } else if(shift & E0ESC){
-////     // Last character was an E0 escape; or with 0x80
-////     data |= 0x80;
-////     shift &= ~E0ESC;
-////   }
-////
-////   shift |= shiftcode[data];
-////   shift ^= togglecode[data];
-////   c = charcode[shift & (CTL | SHIFT)][data];
-////   if(shift & CAPSLOCK){
-////     if('a' <= c && c <= 'z')
-////       c += 'A' - 'a';
-////     else if('A' <= c && c <= 'Z')
-////       c += 'a' - 'A';
-////   }
-////   return c;
-//// }
-////
-//// void
-//// kbdintr(void)
-//// {
-////   consoleintr(kbdgetc);
-//// }
+
+static mut shift: u8 = 0;
+const charcode: [&[u8; 256]; 4] = [&normalmap, &shiftmap, &ctlmap, &ctlmap];
+pub unsafe fn kbdgetc() -> i32 {
+    let st = inb(KBSTATP as u16);
+    if ((st & KBS_DIB) == 0) {
+        return -1;
+    }
+    let mut data = inb(KBDATAP as u16);
+
+    if (data == 0xE0) {
+        shift |= E0ESC;
+        return 0;
+    } else if ((data & 0x80) != 0) {
+        // Key released
+        data = if (shift & E0ESC) != 0 {
+            data
+        } else {
+            data & 0x7F
+        };
+        shift &= !(shiftcode(data) | E0ESC);
+        return 0;
+    } else if (shift & E0ESC) != 0 {
+        // Last character was an E0 escape; or with 0x80
+        data |= 0x80;
+        shift &= !E0ESC;
+    }
+
+    shift |= shiftcode(data);
+    shift ^= togglecode(data);
+    let mut c = charcode[(shift & (CTL | SHIFT)) as usize][data as usize];
+    if (shift & CAPSLOCK) != 0 {
+        if (b'a' <= c && c <= b'z') {
+            c += b'A' - b'a';
+        } else if (b'A' <= c && c <= b'Z') {
+            c += b'a' - b'A';
+        }
+    }
+    return c as i32;
+}
+
+pub unsafe fn kbdintr() {
+    consoleintr(kbdgetc);
+}
